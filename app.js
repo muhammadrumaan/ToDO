b29ff7f631cd14b3b40291883b5b7c17a359fddf
Muhammad Rumaan <96781150+muhammadrumaan@users.noreply.github.com>
2022-02-25 18:47:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let notesObj= prevnote || [];
 let toDoAdd=document.querySelector('.todo-add-container');
 toDoAdd.innerHTML=html;
 let noNotes=document.querySelector('.nonotes-txt');
-if (prevnote.length==null || prevnote==null) {
+if ( prevnote==null) {
   noNotes.innerHTML=`<h3><em>NO ToDo's, Add One</em></h3>`;
 }
 addBtn.addEventListener('click',function(element) {
